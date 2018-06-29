8004d247f2f4ed240728a511d1958d549e3f838e
Leandro Parra <nixlovemi@gmail.com>
2018-06-28 21:45:31 -0300

    Update etiqueta.php

--- application/views/Produto/etiqueta.php
+++ application/views/Produto/etiqueta.php
@@ -9,14 +9,14 @@ $proCodigo = isset($Produto["pro_codigo"]) ? $Produto["pro_codigo"]: "";
 $proEan    = isset($Produto["ean"]) ? $Produto["ean"]: "0000000000000";
 // =========
 
-$pdf = new FPDF('L','mm',array(17, 34));
+$pdf = new FPDF('L','mm',array(24, 111));
 $pdf->Open();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 
 $pdf->SetMargins(0,0);
 $pdf->SetX(0);
-$pdf->SetY(0);
+$pdf->SetY(1.5);
 $pdf->SetAutoPageBreak(true, 2);
 $pdf->SetFillColor(255, 255, 255);
 $pdf->SetFont('Arial', '', 8);
@@ -28,13 +28,21 @@ $vBarCode = $proEan;
 $bar  = new BARCODE();
 $bar->BarCode_save('EAN-13', $vBarCode, $nome, $path, 'jpeg', 50, 2, "#FFFFFF", "#000000", true);
 $vNomeArq = $path.$nome.'.jpeg';
-$pdf->Image($vNomeArq, 1, null, 35, 10, 'jpeg');
-$pdf->Ln(1);
-unlink($vNomeArq);
 
+$pdf->Image($vNomeArq, 3, null, 35, 16, 'jpeg');
 $pdf->SetFont('Arial', '', 7);
-$pdf->Cell(34, 4, utf8_decode("ID $proId - Cód: $proCodigo"), 0, 0, 'C', true);
+$pdf->Text(10, 20, utf8_decode("ID $proId - Cód: $proCodigo"));
+
+$pdf->SetY(1.5);
+$pdf->Image($vNomeArq, 38, null, 35, 16, 'jpeg');
+$pdf->Text(48, 20, utf8_decode("ID $proId - Cód: $proCodigo"));
+
+$pdf->SetY(1.5);
+$pdf->Image($vNomeArq, 75, null, 35, 16, 'jpeg');
+$pdf->Text(86, 20, utf8_decode("ID $proId - Cód: $proCodigo"));
+
 $pdf->Ln(4);
+unlink($vNomeArq);
 
 // $pdf->SetFont('Arial', '', 7);
 // $pdf->MultiCell(34, 4, utf8_decode('Nome do Produto é Muito Grande mas não temos o que fazer'));
