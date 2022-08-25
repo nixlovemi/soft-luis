a7a40c039448191b2ab736abfe7917dc78705d83
Leandro Parra <nixlovemi@gmail.com>
2022-08-24 22:08:33 -0400

    chore: dynamic protocol

--- application/config/config.php
+++ application/config/config.php
@@ -28,7 +28,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "https://" . $_SERVER['HTTP_HOST'] . "/";
+if (isset($_SERVER['HTTPS']) &&
+    ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $protocol = 'https://';
+}
+else {
+  $protocol = 'http://';
+}
+
+$config['base_url'] = $protocol . $_SERVER['HTTP_HOST'] . "/";
 
 /*
 |--------------------------------------------------------------------------
